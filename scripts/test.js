0d96aa0bab69119f388bd8128cbbbda6463310bf
frangaar <124348005+frangaar@users.noreply.github.com>
2024-01-19 17:12:25 +0100

    alert

--- scripts/test.js
+++ scripts/test.js
@@ -1,18 +1,26 @@
 window.onload = () => {
 
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+            if (mobile) {
+                alert("Visit this on a Computer for Better View");              
+            } else {
+        
+            }
+
     function notifyMe() {
 
-        Notification.requestPermission()
-    
+        Notification.requestPermission();
+
+       
+        
         // Let's check if the browser supports notifications
         if (!("Notification" in window)) {
           console.log("This browser does not support desktop notification");
-          alert("no puedo");
         } 
         // Let's check whether notification permissions have alredy been granted
         else if (Notification.permission === "granted") {
           // If it's okay let's create a notification
-          alert("puedo");
+
           var notification = new Notification("Alerta!",{
             icon:"https://i.pinimg.com/originals/ae/e2/3f/aee23f579754fe36ea8cc2643597ca5a.jpg",
             body:"Movimiento en la ENTRADA",
