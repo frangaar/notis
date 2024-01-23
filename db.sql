0cca24acf28cec6da802b26510349122deec928d
frangaar <124348005+frangaar@users.noreply.github.com>
2024-01-23 20:17:42 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -11,6 +11,15 @@ rider varchar(250) not null,
 leido boolean
 );
 
+tabla riders
+tabla proveedores
+tabla centro sociales
+tabla menus (info menus)
+tabla pedidos (id, rider,proveedor,cantidad packs)
+tabla favoritos
+tabla puntos_entrega
+tabla notis_usuario
+
 insert into notis_usuario(id,mensaje,rider,leido) values(null,"Prueba mensaje","rid1",false);
 
 insert into notis_usuario(id,mensaje,rider,leido) values(null,"Prueba mensaje","rid1",false);
